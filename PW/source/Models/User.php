3584a6147a6283eb300d477e9037494a217ab728
Murilo Fazenda Rech <murilofazendarech777@gmail.com>
2026-03-11 16:29:35 -0300

    Add files via upload

--- PW/source/Models/User.php
+++ PW/source/Models/User.php
@@ -4,12 +4,19 @@ namespace source\Models;
 
 class User
 {
+
+    public $id;
     private $name;
     private $email;
+
+
     public function __construct(
+        int $id,
         string $name = null,
-        string $email = null)
+        string $email = null,
+        )
     {
+        $this->id = $id;
         $this->name = $name;
         $this->email = $email;
     }
@@ -22,5 +29,28 @@ class User
     public function setName(string $name = null): void
     {
         $this->name = $name;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email = null): void
+    {
+        $this->email = $email;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function show(){
+        echo "Usuário: #{$this->id} - Nome: {$this->name} - Email: {$this->email}";
     }
 }
