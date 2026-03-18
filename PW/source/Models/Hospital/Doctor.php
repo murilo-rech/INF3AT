267222f2823605a67d1099c46bbd62d591636749
Murilo Fazenda Rech <murilofazendarech777@gmail.com>
2026-03-18 16:39:15 -0300

    Add files via upload

---
+++ PW/source/Models/Hospital/Doctor.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace source\Models\Hospital;
+
+class Doctor extends \source\models\User {
+    private $crm;
+    private $specialty;
+
+    public function __construct(int $id = null, string $name = null, string $email = null, string $password = null, string $crm = null, string $specialty = null) {
+        parent::__construct($id, $name, $email, $password);
+        $this->crm = $crm;
+        $this->specialty = $specialty;
+    }
+
+    public function getCrm(): string {
+        return $this->crm;
+    }
+
+    public function getSpecialty(): string {
+        return $this->specialty;
+    }
+
+    public function setSpecialty(string $specialty): void{
+        $this->specialty = $specialty;
+    }
+    
+    public function setCrm(string $crm): void {
+        $this->crm = $crm;
+    }
+
+    public function show(): void{
+        echo "<br>Médico: #{$this->getId()} - Nome: {$this->getName()} <br>
+            Email: {$this->getEmail()} <br>
+            CRM: {$this->getCrm()} <br>
+            Especialidade: {$this->getSpecialty()} <br>";
+    }
+}
